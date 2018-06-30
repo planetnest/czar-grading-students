59e9c129b2c1b886896da7545d3ad642120e042d
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-06-30 21:14:55 +0100

    Tests written

---
+++ test.js
@@ -0,0 +1,20 @@
+describe('Grading system test', () => {
+  const grad3 = require('./');
+
+  it('leaves < 38 numbers unchanged', () => {
+    expect(grade(38)).toBe(38);
+    expect(grade(29)).toBe(29);
+  });
+
+  it('rounds to nearest multiple of 5', () => {
+    expect(grade(84)).toBe(85);
+  });
+
+  it('returns array for array input', () => {
+    expect(grade([])).toBeInstanceOf(Array);
+  });
+
+  it('perfectly processes array of grades', () => {
+    expect(grade([73,67,38,33])).toEqual([75,67,40,33]);
+  });
+});
