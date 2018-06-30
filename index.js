42123058550df66bbdbcfa6cc7f7c06248287c7e
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-06-30 21:25:12 +0100

    First implementation, should proof the round() implementation

---
+++ index.js
@@ -0,0 +1,19 @@
+const gradeList = (list) => {
+
+}
+
+const round = score => {
+  let m = score % 5;
+  if (m == 0 || 5-m >= 3) return score;
+  let d = Math.floor(score / 5);
+  return (d+1) * 5;
+}
+
+const grade = (score) => {
+  if (score < 38) return score;
+  return round(score);
+}
+
+module.exports = function(input) {
+  return Array.isArray(input) ? gradeList(input) : grade(input);
+}
